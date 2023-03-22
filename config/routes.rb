6b1bf558724d463cabd839da333d846dbaf9d481
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-22 14:29:44 +0300

    Routes cont

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,14 @@ Rails.application.routes.draw do
   # GET a specific restaurant and its associated pizzas
   get '/restaurants/:id', to: 'restaurants#show'
 
- 
+  # DELETE a specific restaurant and its associated restaurant_pizzas
+  delete '/restaurants/:id', to: 'restaurants#destroy'
+
+  # GET all pizzas
+  get '/pizzas', to: 'pizzas#index'
+
+  # POST a new restaurant_pizza
+  post '/restaurant_pizzas', to: 'restaurant_pizzas#create'
 
   # Defines the root path route ("/")
   # root "articles#index"
