c0e1eed2979e67a2e9f9f4e5468f2e27300fcbfe
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-22 14:27:33 +0300

    Routes get

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # GET all restaurants
+  get '/restaurants', to: 'restaurants#index'
+
+  # GET a specific restaurant and its associated pizzas
+  get '/restaurants/:id', to: 'restaurants#show'
+
+ 
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
