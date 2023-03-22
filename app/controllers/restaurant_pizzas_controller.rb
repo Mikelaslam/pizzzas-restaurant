3139fe79b173a684ac08aafaa5430303b0011d87
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-22 21:25:23 +0300

    create restaurant_pizzas

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,11 @@
 class RestaurantPizzasController < ApplicationController
+    def create
+  restaurant_pizza = RestaurantPizza.new(restaurant_pizza_params)
+  if restaurant_pizza.save
+    render json: { id: restaurant_pizza.pizza.id, name: restaurant_pizza.pizza.name, ingredients: restaurant_pizza.pizza.ingredients }
+  else
+    render json: { errors: restaurant_pizza.errors.full_messages }, status: :unprocessable_entity
+  end
+end
+
 end
