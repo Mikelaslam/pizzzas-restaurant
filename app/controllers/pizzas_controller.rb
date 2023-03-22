86fda129b12ee2a81a1b44a045214ecd3db50494
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-22 21:23:29 +0300

    Pizzas controller

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,3 +1,7 @@
 class PizzasController < ApplicationController
-    
+    def index
+  pizzas = Pizza.all
+  render json: pizzas.map { |p| { id: p.id, name: p.name, ingredients: p.ingredients } }
+end
+
 end
