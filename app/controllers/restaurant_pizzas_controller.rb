c6c0e320de2bd51c482c1389ea92eb387659f575
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-22 21:41:13 +0300

    create params

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -8,4 +8,10 @@ class RestaurantPizzasController < ApplicationController
   end
 end
 
+private
+
+def restaurant_pizza_params
+  params.require(:restaurant_pizza).permit(:price, :pizza_id, :restaurant_id)
+end
+
 end
