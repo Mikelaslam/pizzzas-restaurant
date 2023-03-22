241291b9d7c99292fc41a5e87ca4ab7654d4d9be
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-22 21:21:59 +0300

    Def show restaurant

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -3,5 +3,18 @@ class RestaurantController < ApplicationController
   restaurants = Restaurant.all
   render json: restaurants.map { |r| { id: r.id, name: r.name, address: r.address } }
 end
+def show
+  restaurant = Restaurant.find_by(id: params[:id])
+  if restaurant
+    render json: {
+      id: restaurant.id,
+      name: restaurant.name,
+      address: restaurant.address,
+      pizzas: restaurant.pizzas.map { |p| { id: p.id, name: p.name, ingredients: p.ingredients } }
+    }
+  else
+    render json: { error: "Restaurant not found" }, status: :not_found
+  end
+end
 
 end
